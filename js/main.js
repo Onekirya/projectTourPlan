f00d08f925ec33a6fae9c5b6270ae2e30671d921
onekirya <kirill0007@mail.ru>
2022-02-14 16:49:30 +0300

    create: added swiper-slider in hotel section

---
+++ js/main.js
@@ -0,0 +1,11 @@
+var swiper = new Swiper('.swiper', {
+  // Optional parameters
+  loop: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.slider-button--next',
+    prevEl: '.slider-button--prev',
+  },
+  effect: "fade",
+});
